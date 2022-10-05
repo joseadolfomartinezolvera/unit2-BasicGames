5b2d06c6638ae2143cf394c12eecfa6c37fdd911
joseadolfomartinezolvera <56425103+joseadolfomartinezolvera@users.noreply.github.com>
2022-10-04 20:27:44 -0500

    Update DestroyOutOfBounds.cs

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
 
-    private float topBound = 30;
-    private float lowerBound = -10;
+    private float topBound = 30;    //Variable que inidica el limite superior al que puede llegar la comidad 
+    private float lowerBound = -10; //variable que indica el limite interior al que pueden llegar los animales 
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +19,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    //parte encargada de destruir los GameObject cuando estos don > o < dependiendo si es animal o su comida
       if (transform.position.z > topBound)
       {
           Destroy(gameObject);
       } else if (transform.position.z < lowerBound)
       {
+           //muestra un pequeño mensaje de "fin del juego"
           Debug.Log("Fin del Juego");
           Destroy(gameObject);
       }
